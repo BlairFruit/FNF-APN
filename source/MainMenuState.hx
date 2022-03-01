9e8ba7e0f08143d3a9c2cc58fcc330443dff2c32
MonochromeFNFModder <96321264+MonochromeFNFModder@users.noreply.github.com>
2022-03-01 18:35:45 -0300

    how the fuck are we going to make the menu change

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -35,9 +35,9 @@ class MainMenuState extends MusicBeatState
 	var optionShit:Array<String> = [
 		'story_mode',
 		'freeplay',
-		#if MODS_ALLOWED 'mods', #end
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
-		'credits',
+	//	#if MODS_ALLOWED 'mods', #end
+	//	#if ACHIEVEMENTS_ALLOWED 'awards', #end
+	//	'credits',
 		#if !switch 'donate', #end
 		'options'
 	];
