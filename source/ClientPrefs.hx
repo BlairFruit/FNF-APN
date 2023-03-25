e45b1b26cea1484c83387173b9d4066d0c13f8c9
MonochromeDEV <96321264+Monochrome-DEV@users.noreply.github.com>
2023-03-25 15:30:10 -0300

    folder creation pt. 1
    made folderstate, made foldered clientprefs variable

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -7,6 +7,7 @@ import flixel.graphics.FlxGraphic;
 import Controls;
 
 class ClientPrefs {
+	public static var foldered:Bool = false;
 	public static var downScroll:Bool = false;
 	public static var middleScroll:Bool = false;
 	public static var showFPS:Bool = true;
@@ -91,6 +92,7 @@ class ClientPrefs {
 	}
 
 	public static function saveSettings() {
+		FlxG.save.data.foldered = foldered;
 		FlxG.save.data.downScroll = downScroll;
 		FlxG.save.data.middleScroll = middleScroll;
 		FlxG.save.data.showFPS = showFPS;
@@ -133,6 +135,9 @@ class ClientPrefs {
 	}
 
 	public static function loadPrefs() {
+		if(FlxG.save.data.foldered != null) {
+			foldered = FlxG.save.data.foldered;
+		}
 		if(FlxG.save.data.downScroll != null) {
 			downScroll = FlxG.save.data.downScroll;
 		}
---
+++ source/FolderState.hx
@@ -0,0 +1,62 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.effects.FlxFlicker;
+import lime.app.Application;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import Sys;
+
+class FolderState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	var warnText:FlxText;
+	override function create()
+	{
+		super.create();
+		var username:String = Sys.environment()["COMPUTERNAME"];
+
+		Sys.createDirectory("C:/Users/"+username+"/Desktop/IMSCARED/FNF");
+		
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+
+		warnText = new FlxText(0, 0, FlxG.width,
+			"Hello "+username+".\n
+			A Pixelated Nightmare has created a folder inside your desktop\n
+			DO NOT Delete this folder, as it will be detrimental to the game\n
+			Press Enter to continue",
+			32);
+		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		warnText.screenCenter(Y);
+		add(warnText);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if(!leftState) {
+			if (controls.ACCEPT) {
+				leftState = true;
+				FlxTransitionableState.skipNextTransIn = true;
+				FlxTransitionableState.skipNextTransOut = true;
+				if(!back) {
+					ClientPrefs.foldered = true;
+					ClientPrefs.saveSettings();
+					FlxG.sound.play(Paths.sound('confirmMenu'));
+					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
+						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
+							MusicBeatState.switchState(new TitleState());
+						});
+					});
+				}
+			}
+		}
+		super.update(elapsed);
+	}
+}
