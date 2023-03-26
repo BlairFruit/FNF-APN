6209c4d2e7d5fad7d6df25dfd15cebb222ae7f85
MonochromeDEV <96321264+Monochrome-DEV@users.noreply.github.com>
2023-03-26 00:31:23 -0300

    temporary savebind

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -142,7 +142,7 @@ class TitleState extends MusicBeatState
 		swagShader = new ColorSwap();
 		super.create();
 
-		FlxG.save.bind('funkin', 'ninjamuffin99');
+		FlxG.save.bind('FNFAPN', 'WhiteFace');
 
 		if(!initialized && FlxG.save.data != null && FlxG.save.data.fullscreen)
 		{
